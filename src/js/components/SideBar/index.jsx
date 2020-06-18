1d15a4158749070d6e5e546be169e33ff7e5d6ce
ImNotMee <philip200156@hotmail.com>
2020-06-18 15:31:51 -0400

    update to side bar with styling

--- src/js/components/SideBar/index.jsx
+++ src/js/components/SideBar/index.jsx
@@ -4,38 +4,43 @@ import './styles.css';
 class Sidebar extends Component {
   constructor(props) {
       super(props);
+      this.userID = "thisisjohn1";
+      this.name = "John Doe";
+      this.profilePic = "https://3.bp.blogspot.com/-qDc5kIFIhb8/UoJEpGN9DmI/AAAAAAABl1s/BfP6FcBY1R8/s1600/BlueHead.jpg";
   }
 
 
     render() {
       return (
-        <div id="sidebar">
-          <div>
-            <img id="userprofile" src="https://media-exp1.licdn.com/dms/image/C560BAQFHd3L0xFcwcw/company-logo_200_200/0?e=2159024400&v=beta&t=n0t8JMKRzeZtfUr1dh_p_JqyJEuhnwPHD8LJ0p1L_Tg"></img>
-            <h2>Name</h2>
-            <h3>UserID</h3>
+        <div id="sideBar">
+          <div id="userProfile">
+            <img id="userPicture" src={this.profilePic}></img>
+            <h2>{this.name}</h2>
+            <h3>{this.userID}</h3>
           </div>
 
-          <div id="navlink">
-            <img id="icon" src="https://media-exp1.licdn.com/dms/image/C560BAQFHd3L0xFcwcw/company-logo_200_200/0?e=2159024400&v=beta&t=n0t8JMKRzeZtfUr1dh_p_JqyJEuhnwPHD8LJ0p1L_Tg"></img>
+          <div id="navLink">
+            <img id="icon" src="https://image.flaticon.com/icons/svg/3062/3062317.svg"></img>
             <a id="links" href="">Overview</a>
           </div>
-          <div id="navlink">
-            <img id="icon" src="https://media-exp1.licdn.com/dms/image/C560BAQFHd3L0xFcwcw/company-logo_200_200/0?e=2159024400&v=beta&t=n0t8JMKRzeZtfUr1dh_p_JqyJEuhnwPHD8LJ0p1L_Tg"></img>
+          <div id="navLink">
+            <img id="icon" src="https://image.flaticon.com/icons/svg/1720/1720256.svg"></img>
             <a id="links" href="">Trend</a>
           </div>
-          <div id="navlink">
-            <img id="icon" src="https://media-exp1.licdn.com/dms/image/C560BAQFHd3L0xFcwcw/company-logo_200_200/0?e=2159024400&v=beta&t=n0t8JMKRzeZtfUr1dh_p_JqyJEuhnwPHD8LJ0p1L_Tg"></img>
+          <div id="navLink">
+            <img id="icon" src="https://image.flaticon.com/icons/svg/1792/1792756.svg"></img>
             <a id="links" href="">Remainders</a>
           </div>
-          <div id="navlink">
-            <img id="icon" src="https://media-exp1.licdn.com/dms/image/C560BAQFHd3L0xFcwcw/company-logo_200_200/0?e=2159024400&v=beta&t=n0t8JMKRzeZtfUr1dh_p_JqyJEuhnwPHD8LJ0p1L_Tg"></img>
+          <div id="navLink">
+            <img id="icon" src="https://image.flaticon.com/icons/svg/747/747310.svg"></img>
             <a id="links" href="">Calendar</a>
           </div>
-          <div id="navlink">
-            <img id="icon" src="https://media-exp1.licdn.com/dms/image/C560BAQFHd3L0xFcwcw/company-logo_200_200/0?e=2159024400&v=beta&t=n0t8JMKRzeZtfUr1dh_p_JqyJEuhnwPHD8LJ0p1L_Tg"></img>
+          <div id="navLink">
+            <img id="icon" src="https://image.flaticon.com/icons/svg/684/684809.svg"></img>
             <a id="links" href="">Check-In</a>
           </div>
+
+          <button id="logOut" onclick="">logout</button>
         </div>
       )}
 }
