baf4295df7e1343ccc683b0b4ea2ed2243c9caa6
calebdsouza <caleb.dsouza@mail.utoronto.ca>
2020-06-19 00:07:37 -0400

    update dev tools

--- src/js/components/LandingPage/Login/index.jsx
+++ src/js/components/LandingPage/Login/index.jsx
@@ -1,55 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
-import './styles.css';
+import './styles.css'
 
 class LoginForm extends Component {
-
     constructor(props) {
-        super(props);
-        this.state= {
+        super(props)
+        this.state = {
             email: '',
             password: '',
-            invalidLogin: false
-        };
+            invalidLogin: false,
+        }
     }
 
     // TODO: temp hack to force render
     componentDidUpdate(props) {
         if (props.invalidLoginStatus !== this.props.invalidLoginStatus) {
             this.setState({
-                invalidLogin: props.invalidLoginStatus
+                invalidLogin: props.invalidLoginStatus,
             })
         }
     }
 
     onInputChangeHandler = (event) => {
-        const TARGET = event.target;
+        const TARGET = event.target
         this.setState({
             [TARGET.name]: TARGET.value,
-        });
+        })
     }
 
     render() {
-        return (  
+        return (
             <div id="LoginFrom">
-                <input className={`text-input ${this.props.invalidLoginStatus ? 'invalidInput' : ''}`}
-                       type="email" 
-                       value={this.state.email}
-                       onChange={this.onInputChangeHandler}
-                       placeholder="email"
-                       name="email"
+                <input
+                    className={`text-input ${
+                        this.props.invalidLoginStatus ? 'invalidInput' : ''
+                    }`}
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.onInputChangeHandler}
+                    placeholder="email"
+                    name="email"
                 />
-                <input className={`text-input ${this.props.invalidLoginStatus ? 'invalidInput' : ''}`}
-                       type="password" 
-                       value={this.state.password} 
-                       onChange={this.onInputChangeHandler}
-                       name="password"
-                       placeholder="password"
+                <input
+                    className={`text-input ${
+                        this.props.invalidLoginStatus ? 'invalidInput' : ''
+                    }`}
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.onInputChangeHandler}
+                    name="password"
+                    placeholder="password"
                 />
-                <button className="primary-btn" onClick={() => {this.props.loginHandler(this.state.email, this.state.password)}}> login </button>
+                <button
+                    className="primary-btn"
+                    onClick={() => {
+                        this.props.loginHandler(
+                            this.state.email,
+                            this.state.password
+                        )
+                    }}
+                >
+                    {' '}
+                    login{' '}
+                </button>
             </div>
-        );
+        )
     }
 }
 
-export default LoginForm;
+export default LoginForm
