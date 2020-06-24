301c28ddb0b863ae17c7ae2d6ab9c2da7c3c6085
byang123 <brianyang0721@gmail.com>
2020-06-24 15:33:29 -0400

    Mental card component completed

--- src/js/components/Overview/index.jsx
+++ src/js/components/Overview/index.jsx
@@ -1,42 +1,166 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Card from '../Card';
+import { fetchCardData } from '../../actions/cardData';
 
 class Overview extends Component {
   constructor(props) {
     super(props);
+
+    //server call to initialize state
+    //we can also set up state and use componentDidMount to update values
+    //user = {user: this.props.activeUser}
+    //user_data = await fetchCardData(this.props.activeUser)
+    //this.state = Object.assign(user, user_data)
   }
 
   state = {
     user: this.props.activeUser,
+    BMI: {
+      value: 22,
+    },
+    Water: {
+      completed: 0.8,
+      remaining: 2,
+      unit: 'L',
+    },
+    Calories: {
+      completed: 300,
+      remaining: 1700,
+      unit: 'Calories',
+    },
+    Mood: {
+      value: 'happy',
+    },
+    Sleep: {
+      hours: 8,
+      quality: 'Good', // 3 levels bad, okay, good
+    },
+    Stress: {
+      value: 1,
+    },
+    Medication: [
+      {
+        drug: 'Cold Medicine',
+        completed: 1,
+        remainging: 2,
+      },
+      {
+        drug: 'Allergy Medicine',
+        completed: 2,
+        remainging: 4,
+      },
+    ],
+    Sickness: {
+      //
+      sick: true,
+    },
+    Appointments: [
+      // shows all appointments today
+      {
+        event: 'Annual Checkup',
+        doctor: 'Dr. Zoudas',
+        time: '16:00:00',
+      },
+      {
+        event: 'Blood Donation',
+        doctor: 'Dr. Dre',
+        time: '10:00:00',
+      },
+      {
+        event: 'Dentist',
+        doctor: 'Dr. Teth',
+        time: '12:00:00',
+      },
+    ],
   };
+
   render() {
     return (
       <div id="OverviewWrapper">
         <div id="cardsContainer">
+          {/* physical tracking */}
           <Card
             id="cards"
             activeUser={this.props.activeUser}
             title="Body Mass Index"
-            value="22"
+            value={this.state.BMI}
             image="https://image.flaticon.com/icons/svg/3023/3023711.svg"
+            type="1"
           />
           <Card
             id="cards"
             activeUser={this.props.activeUser}
             title="Water Consumption"
-            value={[0.8, 2, 'L']}
+            value={this.state.Water}
             image=""
+            type="2"
           />
           <Card
             id="cards"
             activeUser={this.props.activeUser}
             title="Calories"
-            value={[1700, 300, 'Calories']}
+            value={this.state.Calories}
             image=""
+            type="3"
           />
         </div>
-        <div id="cardsContainer"></div>
+
+        <div id="cardsContainer">
+          {/* mental tracking */}
+          <Card
+            id="cards"
+            activeUser={this.props.activeUser}
+            title="Mood"
+            value={this.state.Mood}
+            image=""
+            type="4"
+          />
+          <Card
+            id="cards"
+            activeUser={this.props.activeUser}
+            title="Sleep"
+            value={this.state.Sleep}
+            image=""
+            type="5"
+          />
+          <Card
+            id="cards"
+            activeUser={this.props.activeUser}
+            title="Stress"
+            value={this.state.Stress}
+            image=""
+            type="6"
+          />
+        </div>
+
+        <div id="cardsContainer">
+          {/* medical tracking */}
+          <Card
+            id="cards"
+            activeUser={this.props.activeUser}
+            title="Medication"
+            value={this.state.Medication}
+            image=""
+            type="7"
+          />
+          <Card
+            id="cards"
+            activeUser={this.props.activeUser}
+            title="Sickness"
+            value={this.state.Sickness}
+            image=""
+            type="8"
+          />
+          <Card
+            id="cards"
+            activeUser={this.props.activeUser}
+            title="Appointments"
+            value={this.state.Appointments}
+            image=""
+            type="9"
+          />
+        </div>
       </div>
     );
   }
