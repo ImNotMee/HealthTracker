0126f8e479e87370fb8c37584738bf3e9dcdde00
ImNotMee <philip200156@hotmail.com>
2020-06-25 13:52:05 -0400

    Revert "Revert "Merge pull request #14 from csc309-summer-2020/check-in""
    This reverts commit 3b06d41dcfe40a240157979aeab0a7a112bbf3bc.

--- src/js/components/Reminders/index.jsx
+++ src/js/components/Reminders/index.jsx
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class Reminders extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   state = {
     user: this.props.activeUser,
   };
+
   render() {
     return <div id="RemindersWrapper">some reminder text and stuff</div>;
   }
