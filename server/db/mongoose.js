af820a67b2f0e5e957e752086d0507cf2f0996e1
calebdsouza <caleb.dsouza@mail.utoronto.ca>
2020-07-31 23:29:20 -0400

    aded inistal mongoose db connection code

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+/* Connnect to our database */
+// Get the URI of the local database, or the one specified on deployment.
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Users';
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .catch((error) => {
+    console.log('Error connecting to mongodb. Timeout reached.', error);
+  });
+
+module.exports = { mongoose }; // Export the active connection.
