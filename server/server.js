0a374175dbaa866ca0b79cea09a6d490193b8239
calebdsouza <caleb.dsouza@mail.utoronto.ca>
2020-08-02 16:45:31 -0400

    Added startup for client in start script

--- server/server.js
+++ server/server.js
@@ -34,6 +34,44 @@ function isMongoError(error) {
 //static public directory for the files in /public
 app.use(express.static(__dirname + '../public'));
 
+// Serve the build
+app.use(express.static(path.normalize(__dirname + '/..//client/build')));
+
+// All routes other than above will go to index.html
+app.get('*', (req, res) => {
+  const goodPageRoutes = [
+    '/',
+    '/signup',
+    '/settings',
+    '/overview',
+    '/trends',
+    '/reminders',
+    '/calendar',
+    '/check-in',
+    '/overview/logWeight',
+    '/overview/logWater',
+    '/overview/logCalories',
+    '/overview/logMood',
+    '/overview/logSleep',
+    '/overview/logStress',
+    '/overview/logMedical',
+    '/overview/logSick',
+    '/reminders/add/:cat?/:sub?/:name?/:time?/:note?/:id?',
+    '/manage-users',
+    '/trends/admin',
+    '/alert-system',
+    '/locations/add',
+    '/user-profile/:id?',
+    '/alert-system/add/:name?/:addr?/:img?/:maxOcc?/:desc?',
+  ];
+
+  if (!goodPageRoutes.includes(req.url)) {
+    res.status(404);
+  }
+
+  res.sendFile(path.normalize(__dirname + '/..//client/build/index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   log(`Listening on port ${port}...`);
