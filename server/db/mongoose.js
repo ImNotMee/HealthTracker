058535293cca27bc29bdceafa1a20dd3e21ccb7a
calebdsouza <caleb.dsouza@mail.utoronto.ca>
2020-08-03 22:53:00 -0400

    fixed db name

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Users';
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/HealthTrackerApp';
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
