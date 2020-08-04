d0522ccd99e27c0a46edda42f0ccf6e2ebbb4565
calebdsouza <caleb.dsouza@mail.utoronto.ca>
2020-08-03 23:00:23 -0400

    login now works on backend api auth calls

--- client/src/js/actions/login.js
+++ client/src/js/actions/login.js
@@ -1,5 +1,26 @@
 const log = console.log;
 
+export const readCookie = (app) => {
+  log('Reading cookie...');
+  const url = 'http://localhost:5000/auth/session';
+
+  fetch(url)
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json();
+      }
+    })
+    .then((json) => {
+      if (json && json.activeUser) {
+        setActiveUser(app, json.activeUser);
+      }
+      log('Cookie read');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 export const setActiveUser = (app, user) => {
   app.setState(
     {
@@ -12,36 +33,41 @@ export const setActiveUser = (app, user) => {
 };
 
 export const onLoginHandler = (landingPage, email, password) => {
-  const authKey = _getUserHash(email, password);
-  const user = landingPage.props.users[authKey];
-  // check login attempt
-  if (user === null || user === undefined) {
-    landingPage.setState({
-      invalidLogin: true,
+  log(email, password);
+  const loginCred = { email: email, password: password };
+  const request = new Request('http://localhost:5000/auth/login', {
+    method: 'post',
+    body: JSON.stringify(loginCred),
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  // Send the request with fetch()
+  fetch(request)
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json();
+      }
+    })
+    .then((res) => {
+      // check login attempt
+      if (res === undefined || res.activeUser === null || res.activeUser === undefined) {
+        landingPage.setState({
+          invalidLogin: true,
+        });
+        log('Invalid aogin attempt. Try again');
+      } else {
+        landingPage.setState({
+          userEmail: email,
+          userPassword: password,
+          invalidLogin: false,
+        });
+        landingPage.props.setActiveUserHandler(res.activeUser);
+      }
+    })
+    .catch((error) => {
+      console.log('Login request failed', error);
     });
-    log('Invalid aogin attempt. Try again');
-  } else {
-    landingPage.setState({
-      userEmail: email,
-      userPassword: password,
-      invalidLogin: false,
-    });
-    landingPage.props.setActiveUserHandler(user);
-  }
-};
-
-export const _getUserHash = (email, password) => {
-  return email + password;
-};
-
-export const getUserByAuthKey = (ctx, authKey) => {
-  const userId = ctx.state.loginUserIdMap[authKey];
-  if (userId === undefined) {
-    return userId;
-  }
-  return getUserById(ctx, userId);
-};
-
-export const getUserById = (ctx, id) => {
-  return ctx.state.users[id];
 };
