a4665d56ad6ed6038c96ddb622eb949cba7bbccf
ImNotMee <philip200156@hotmail.com>
2020-08-04 21:08:43 -0400

    update

--- server/routes/trends.js
+++ server/routes/trends.js
@@ -9,52 +9,105 @@ const log = console.log;
 
 // Get data routes
 router.get('/weight', (req, res) => {
-  const name = "user"
-  User.find({email: name}).then((data) => {
-    // need to change later
-    let weightArr = data[0].trends.weight;
+  const name = req.body.activeUser;
+  User.findOne({email: name}).then((data) => {
+    let weightArr = data.trends.weight.slice(data.trends.weight.length - 8,  data.trends.weight.length - 1);
     res.send({weight: weightArr});
   }).catch((e) => {
-    res.status(500).send('Cannot find weight data');
+    res.status(404).send('Cannot find weight data');
   });
 });
 
 router.get('/sleep', (req, res) => {
-  const name = "user"
-  User.find({email: name}).then((data) => {
-    // need to change later
-    let sleepArr = data[0].trends.sleep;
+  const name = req.body.activeUser;
+  User.findOne({email: name}).then((data) => {
+    let sleepArr = data.trends.sleep;
     res.send({sleep: sleepArr});
   }).catch((e) => {
-    res.status(500).send('Cannot find sleep data');
+    res.status(404).send('Cannot find sleep data');
   });
 });
 
 router.get('/calories', (req, res) => {
-  const name = "user"
-  User.find({email: name}).then((data) => {
-    // need to change later
-    let caloriesArr = data[0].trends.calories;
+  const name = req.body.activeUser;
+  User.findOne({email: name}).then((data) => {
+    let caloriesArr = data.trends.calories;
     res.send({calories: caloriesArr});
   }).catch((e) => {
-    res.status(500).send('Cannot find calories data');
+    res.status(404).send('Cannot find calories data');
   });
 });
 
 router.get('/stress', (req, res) => {
-  const name = "user"
-  User.find({email: name}).then((data) => {
-    // need to change later
-    let sleepArr = data[0].trends.sleep;
+  const name = req.body.activeUser;
+  User.findOne({email: name}).then((data) => {
+    let sleepArr = data.trends.sleep;
     res.send({sleep: sleepArr});
   }).catch((e) => {
-    res.status(500).send('Cannot find sleep data');
+    res.status(404).send('Cannot find sleep data');
   });
 });
 
+
+// Setting data to trends
 router.post('/updateWeight', (req, res) => {
-  const newWeight = req.body.weight;
+  const val = req.body.value;
+  const name = req.body.activeUser;
+
+  User.findOneAndUpdate({email: name}, {"$push": { "trends.weight" : val}},{new: true}).then((user) => {
+    if (!user) {
+      res.status(404).send();
+    } else {
+      res.send({"updated": user.trends.weight});
+    }
+  }).catch((e) => {
+    res.status(404).send('Cannot find data');
+  });
 });
+
+router.post('/updateSleep', (req, res) => {
+  const val = req.body.value;
+  const name = req.body.activeUser;
+
+  User.findOneAndUpdate({email: name}, {"$push": { "trends.sleep" : val}},{new: true}).then((user) => {
+    if (!user) {
+      res.status(404).send();
+    } else {
+      res.send({"updated": user.trends.sleep});
+    }
+  }).catch((e) => {
+    res.status(404).send('Cannot find data');
+  });
+});
+router.post('/updateStress', (req, res) => {
+  const val = req.body.value;
+  const name = req.body.activeUser;
+
+  User.findOneAndUpdate({email: name}, {"$push": { "trends.stress" : val}},{new: true}).then((user) => {
+    if (!user) {
+      res.status(404).send();
+    } else {
+      res.send({"updated": user.trends.stress});
+    }
+  }).catch((e) => {
+    res.status(404).send('Cannot find data');
+  });
+});
+router.post('/updateCalories', (req, res) => {
+  const val = req.body.value;
+  const name = req.body.activeUser;
+
+  User.findOneAndUpdate({email: name}, {"$push": { "trends.calories" : val}},{new: true}).then((user) => {
+    if (!user) {
+      res.status(404).send();
+    } else {
+      res.send({"updated": user.trends.calories});
+    }
+  }).catch((e) => {
+    res.status(404).send('Cannot find data');
+  });
+});
+
 
 
 module.exports = router;
