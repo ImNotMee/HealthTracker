2605d8f2796007f13bd1873b8e5db8fd376be0e6
byang123 <brianyang0721@gmail.com>
2020-08-06 00:55:09 -0400

    fixed card.setstate bug

--- client/src/js/actions/cardData.js
+++ client/src/js/actions/cardData.js
@@ -137,7 +137,7 @@ export const setCalories = (card, newCalories) => {
 
   card.setState({
     user_card: user_card,
-    activeUser: user,
+    user: user,
   });
   console.log(user_card['Calories']['completed']);
 };
