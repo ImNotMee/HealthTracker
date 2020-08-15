2d7a7d3c4d4ebdde30a8ef4a7b58ff9fc8e0d37e
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2020-08-15 08:18:29 -0400

    small error fixed

--- client/src/js/actions/logout.js
+++ client/src/js/actions/logout.js
@@ -1,7 +1,6 @@
 const log = console.log;
 export const logoutUser = (ctx) => {
   log('Logging out user and saving details...');
-  const activeUser = ctx.state.activeUser;
   ctx.setState(
     {
       activeUser: null,
